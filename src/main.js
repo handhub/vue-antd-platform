33117fc961cb59938da694988025762fe8e41f1b
亓鸿阳 <qihongyang@dingtalk.com>
2019-07-11 13:34:19 +0800

    脚手架搭建

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
+import { Button } from 'ant-design-vue';
 import App from './App.vue';
 import router from './router';
 
+Vue.component(Button.name, Button);
 Vue.config.productionTip = false;
 
 new Vue({
