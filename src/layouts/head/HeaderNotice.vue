422876a24602ce94495c3463bac0be2e7d4e9827
亓鸿阳 <qihongyang@dingtalk.com>
2019-07-23 09:37:04 +0800

    主页面框架搭建

--- src/layouts/head/HeaderNotice.vue
+++ src/layouts/head/HeaderNotice.vue
@@ -1,36 +1,34 @@
 <template>
   <a-dropdown :trigger="['click']" v-model="visible">
     <div slot="overlay">
-      <!-- <a-spin :spinning="loadding"> -->
-        <a-tabs :tabBarStyle="{textAlign: 'center'}" :animated="{inkBar:true, tabPane:false}"
-        :style="{backgroundColor: 'white', width: '297px'}">
-          <a-tab-pane tab="通知" key="1" :style="{padding: '0 24px'}">
-            <a-list>
-              <a-list-item>
-                <a-list-item-meta title="你收到了 14 份新周报" description="一年前">
-                  <a-avatar style="background-color: white" slot="avatar" src="https://gw.alipayobjects.com/zos/rmsportal/ThXAXghbEsBCCSDihZxY.png"/>
-                </a-list-item-meta>
-              </a-list-item>
-              <a-list-item>
-                <a-list-item-meta title="你推荐的 曲妮妮 已通过第三轮面试" description="一年前">
-                  <a-avatar style="background-color: white" slot="avatar" src="https://gw.alipayobjects.com/zos/rmsportal/OKJXDXrmkNshAMvwtvhu.png"/>
-                </a-list-item-meta>
-              </a-list-item>
-              <a-list-item>
-                <a-list-item-meta title="这种模板可以区分多种通知类型" description="一年前">
-                  <a-avatar style="background-color: white" slot="avatar" src="https://gw.alipayobjects.com/zos/rmsportal/kISTdvpyTAhtGxpovNWd.png"/>
-                </a-list-item-meta>
-              </a-list-item>
-            </a-list>
-          </a-tab-pane>
-          <a-tab-pane tab="消息" key="2">
-            暂无消息
-          </a-tab-pane>
-          <a-tab-pane tab="待办" key="3">
-            暂无待办
-          </a-tab-pane>
-        </a-tabs>
-      <!-- </a-spin> -->
+      <a-tabs :tabBarStyle="{textAlign: 'center'}" :animated="{inkBar:true, tabPane:false}"
+      :style="{backgroundColor: 'white', width: '297px'}">
+        <a-tab-pane tab="通知" key="1" :style="{padding: '0 24px'}">
+          <a-list>
+            <a-list-item>
+              <a-list-item-meta title="你收到了 14 份新周报" description="一年前">
+                <a-avatar style="background-color: white" slot="avatar" src="https://gw.alipayobjects.com/zos/rmsportal/ThXAXghbEsBCCSDihZxY.png"/>
+              </a-list-item-meta>
+            </a-list-item>
+            <a-list-item>
+              <a-list-item-meta title="你推荐的 曲妮妮 已通过第三轮面试" description="一年前">
+                <a-avatar style="background-color: white" slot="avatar" src="https://gw.alipayobjects.com/zos/rmsportal/OKJXDXrmkNshAMvwtvhu.png"/>
+              </a-list-item-meta>
+            </a-list-item>
+            <a-list-item>
+              <a-list-item-meta title="这种模板可以区分多种通知类型" description="一年前">
+                <a-avatar style="background-color: white" slot="avatar" src="https://gw.alipayobjects.com/zos/rmsportal/kISTdvpyTAhtGxpovNWd.png"/>
+              </a-list-item-meta>
+            </a-list-item>
+          </a-list>
+        </a-tab-pane>
+        <a-tab-pane tab="消息" key="2">
+          暂无消息
+        </a-tab-pane>
+        <a-tab-pane tab="待办" key="3">
+          暂无待办
+        </a-tab-pane>
+      </a-tabs>
     </div>
     <span @click="fetchNotice" class="header-notice">
       <a-badge count="12">
@@ -50,15 +48,8 @@ export default {
     };
   },
   computed: {
-    // theme() {
-    //   return this.$store.state.setting.layout
-    // === 'side' ? 'light' : this.$store.state.setting.theme;
-    // },
   },
   methods: {
-    callback(key) {
-      window.console.log(key);
-    },
     fetchNotice() {
       if (this.loadding) {
         this.loadding = false;
